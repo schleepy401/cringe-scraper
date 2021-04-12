a63b9f9b6a753a348d95503dcab04745c0f401d8
w0lfw1tz <w0lfw1tz401@gmail.com>
2021-04-13 02:19:56 +0530

    Committed new ref code for use

--- app/redditScraper.py
+++ app/redditScraper.py
@@ -18,10 +18,43 @@ def home():
     permalinks = []
     subReddits = ['your', 'choice', 'of', 'subreddits', 'here']
 
+
+@app.route('/', methods = ['GET'])
+def home():
     for sub in subReddits:
-        myPosts = reddit_obj.subreddit(sub).hot()
+        myPosts = redditwa.subreddit(sub).hot(limit = 10)
         for post in myPosts:
-            if "comments" not in post.url:
+            if ".jpg" in post.url:
                 urls.append(post.url)
+                permalinks.append(post.permalink)
+                titles.append(post.title)
+                authors.append(post.author)
+                structure = { "author": str(authors[len(authors) - 1]), "title": str(titles[len(titles) - 1]), "url": str(urls[len(urls) - 1]), "permalink": str(permalinks[len(permalinks) - 1]) }
+                outputJson.append(structure)
+
+    outputData = { "data": outputJson}
+    return jsonify(outputData)
+
+@app.route('/custom')
+def res():
+    num = request.args.get('n')
+
+    for sub in subReddits:
+        myPosts = redditwa.subreddit(sub).hot(limit = int(num))
+        for post in myPosts:
+            if ".jpg" in post.url:
+                urls.append(post.url)
+                permalinks.append(post.permalink)
+                titles.append(post.title)
+                authors.append(post.author)
+                structure = { "author": str(authors[len(authors) - 1]), "title": str(titles[len(titles) - 1]), "url": str(urls[len(urls) - 1]), "permalink": str(permalinks[len(permalinks) - 1]) }
+                outputJson.append(structure)
+
+    outputData = { "data": outputJson}
+    return jsonify(outputData)
+
+if __name__=="__main__":
+	app.run()
+
 
     return jsonify(urls)
